fb00675ae01be6f435fb14679bf7968972cabe7e
David Mordechai <david.mordechai79@gmail.com>
2022-02-15 14:01:52 +0200

    init commit

---
+++ ConfluentKafkaDemo/ConsumerClient1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.Json;
+using Confluent.Kafka;
+
+var conf = new ConsumerConfig
+{
+    GroupId = "test-consumer-group1",
+    BootstrapServers = "localhost:9092",
+    // Note: The AutoOffsetReset property determines the start offset in the event
+    // there are not yet any committed offsets for the consumer group for the
+    // topic/partitions of interest. By default, offsets are committed
+    // automatically, so in this example, consumption will only start from the
+    // earliest message in the topic 'my-topic' the first time you run the program.
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using var c = new ConsumerBuilder<Ignore, string>(conf).Build();
+c.Subscribe("testTopic");
+
+var cts = new CancellationTokenSource();
+Console.CancelKeyPress += (_, e) => {
+    e.Cancel = true; // prevent the process from terminating.
+    cts.Cancel();
+};
+
+try
+{
+    while (true)
+    {
+        try
+        {
+            var cr = c.Consume(cts.Token);
+            var messageRecord =
+                JsonSerializer.Deserialize<MessageRecord>(cr.Message.Value);
+            Console.WriteLine(
+                $"Consumed message '{messageRecord?.Message}' created at {messageRecord?.MessageDate.ToString("g")}, at: '{cr.TopicPartitionOffset}'.");
+        }
+        catch (ConsumeException e)
+        {
+            Console.WriteLine($"Error occurred: {e.Error.Reason}");
+        }
+    }
+}
+catch (OperationCanceledException)
+{
+    // Ensure the consumer leaves the group cleanly and final offsets are committed.
+    c.Close();
+}
+
+internal record MessageRecord(string Message, DateTime MessageDate);
---
+++ ConfluentKafkaDemo/ProducerApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+using Confluent.Kafka;
+
+var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+// If serializers are not specified, default serializers from
+// `Confluent.Kafka.Serializers` will be automatically used where
+// available. Note: by default strings are encoded as UTF8.
+using var p = new ProducerBuilder<Null, string>(config).Build();
+try
+{
+    while (true)
+    {
+        Console.WriteLine("Produce new message: ");
+        var message = Console.ReadLine();
+        if (message != null)
+        {
+            var messageRecord = new MessageRecord(message, DateTime.Now);
+            var messageJson = JsonSerializer.Serialize(messageRecord);
+            var dr = await p.ProduceAsync("testTopic", new Message<Null, string> { Value = messageJson });
+            Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+        }
+    }
+}
+catch (ProduceException<Null, string> e)
+{
+    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+}
+
+internal record MessageRecord(string Message, DateTime MessageDate = new DateTime());
